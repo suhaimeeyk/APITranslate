e67cd48b26650eb07984a8e0f5b3ea9b09f7c46a
suhaimeeyk <suhaime.yk@rmutsvmail.com>
2022-04-15 00:42:05 +0700

    comit

--- Index-TextClensingToChines.php
+++ Index-TextClensingToChines.php
@@ -15,7 +15,7 @@ CURLOPT_TIMEOUT => 30,
 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 CURLOPT_CUSTOMREQUEST => "GET",
 CURLOPT_HTTPHEADER => array(
-    "Apikey: cTAfAvbmoBdzmEYTUAco602cw4v0FMkq" // key man
+    "Apikey: xxxxxxx" // key man
 )
 ));
 
